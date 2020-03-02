5367b2bb5c532dc70dc4f09ad877a0e0d8be7ab3
chengshaozhi <849910109@qq.com>
2020-03-02 15:14:18 +0800

    ll

--- src/main/java/com/junyang/filter/LoginInterceptor.java
+++ src/main/java/com/junyang/filter/LoginInterceptor.java
@@ -26,7 +26,7 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
     	String token = request.getHeader("token");
-    	System.out.println("***************");
+    	System.out.println("11111");
     	if (token != null) {
     		AdminUserEntity entity = adminUserDao.findByToken(token);
     		if (entity!=null) {
